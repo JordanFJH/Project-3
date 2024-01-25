0c84be86f3486bab602872a67bd8417c1c5b1392
Jordan <higgsfj@gmail.com>
2024-01-25 10:08:50 -0500

    updated test data and added a readme file

--- client/dummyData.js
+++ client/dummyData.js
@@ -1,9 +1,61 @@
+// All in one
+export const allContent = {
+    movieData: [
+        {
+            name: "Movie 1",
+            id: "Movie 1 ID Number",
+            length: "Movie Runtime",
+            progress: "Movie Progress",
+            type: "movie"
+        },
+        {},
+        {}
+    ],
+}
+
 export const movieData = [
     {
-        title: "Movie 1",
-        id: "Movie ID Number",
-        runtime: "Movie Runtime",
-        progress: "Movie Progress"
+        name: "Movie 1",
+        id: "Movie 1 ID Number",
+        length: "Movie Runtime",
+        progress: "Movie Progress",
+        type: "movie"
+    },
+    {},
+    {}
+]
+
+export const tvData = [
+    {
+        name: "Movie 1",
+        id: "Movie 1 ID Number",
+        length: "Movie Runtime",
+        progress: "Movie Progress",
+        type: "tv"
+    },
+    {},
+    {}
+]
+
+export const gameData = [
+    {
+        name: "Movie 1",
+        id: "Movie 1 ID Number",
+        length: "Movie Runtime",
+        progress: "Movie Progress",
+        type: "game"
+    },
+    {},
+    {}
+]
+
+export const bookData = [
+    {
+        name: "Movie 1",
+        id: "Movie 1 ID Number",
+        length: "Movie Runtime",
+        progress: "Movie Progress",
+        type: "book"
     },
     {},
     {}
---
+++ readme.md
@@ -0,0 +1,1 @@
+# Project for project 3

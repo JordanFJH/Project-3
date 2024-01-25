a5fd93abb0660278ab5e174bca6e4ea8829a2cac
Jordan <higgsfj@gmail.com>
2024-01-24 19:41:58 -0500

    first upload of all files

---
+++ client/dummyData.js
@@ -0,0 +1,10 @@
+export const movieData = [
+    {
+        title: "Movie 1",
+        id: "Movie ID Number",
+        runtime: "Movie Runtime",
+        progress: "Movie Progress"
+    },
+    {},
+    {}
+]

059d0ef0751738874ebb0f376630dc3f83b0e059
Jordan <higgsfj@gmail.com>
2024-01-26 18:08:29 -0500

    setup user creation

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,11 +1,25 @@
 const User = require("../models/UserModel")
+const bcrypt = require("bcrypt")
 
 // Creating Account
-
 async function createUser(req, res) {
+    // Check if they exist
+    const foundUser = User.findOne({ username: req.body.username })
+    if (foundUser) {
+        return res.status(400).json({ message: "User already exists" })
+    }
+    // Encrypt password
+    const encryptedPassword = await bcrypt.hash(req.body.password, 10)
+
+    //Updating password and adding to database
+    const newUser = await User.create({...req.body, password: encryptedPassword})
+    console.log(newUser)
+    res.status(200).json({ message: "User has been created" })
 
 }
 
+
+// Logging the user in
 async function loginUser(req, res) {
 
 }

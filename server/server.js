296a20afe7f6c5d79c8bb1a8984db20194acee7b
Jordan <higgsfj@gmail.com>
2024-01-26 17:38:08 -0500

    prelim setup of connecting server to database

--- server/server.js
+++ server/server.js
@@ -1,12 +1,21 @@
 const express = require("express")
+const mongoConfig = require("./config");
+require("dotenv").config()
 
 const app = express()
-
 const PORT = 8080;
+
+const userRoutes = require("./routes/userRoutes");
+
+mongoConfig()
+
+
+// Initial Stuff
+app.use("/api", userRoutes)
 
 
 //MIDDLEWARE
-
+app.use(express.json())
 
 
 app.get("/", (req, res) => {

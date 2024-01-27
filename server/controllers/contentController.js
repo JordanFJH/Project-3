8e38df164bb2d1dc8cbe14c8a220c2f96abf5eb6
Jordan <higgsfj@gmail.com>
2024-01-27 10:38:16 -0500

    setup for adding content to user and content collections

--- server/controllers/contentController.js
+++ server/controllers/contentController.js
@@ -1,3 +1,29 @@
 const User = require("../models/UserModel")
 const Content = require("../models/ContentModel")
 
+
+module.exports.getInfo = async (req, res) => {
+    res.send("You tried to get the collections")
+}
+
+module.exports.addContent = async (req, res) => {
+
+    try {
+        const username = req.header("Username")
+        
+        const media = await Content.create(req.body)
+        await User.findOneAndUpdate({ username: username }, {
+            $push: {
+                content: media._id
+            }
+        })
+        res.status(200).json({ message: "Content Created" })
+        //const foundUser = await User.findOne({ username: username })
+
+
+    } catch (error) {
+        console.log(error.message)
+        res.status(400).json({ message: error.message })
+    }
+
+}
--- server/routes/contentRoutes.js
+++ server/routes/contentRoutes.js
@@ -2,4 +2,10 @@ const express = require("express")
 
 const router = express.Router()
 
+const constCtrl = require("../controllers/contentController")
+
+
+router.get("/", constCtrl.getInfo)
+
+
 module.exports = router

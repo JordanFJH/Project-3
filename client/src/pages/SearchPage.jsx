eefb531fb6d19a6e2fc29b7ed483500e78fce423
Jordan <higgsfj@gmail.com>
2024-01-27 23:41:40 -0500

    setup for search functionality

--- client/src/pages/SearchPage.jsx
+++ client/src/pages/SearchPage.jsx
@@ -1,19 +1,28 @@
+import { useState } from "react";
 
 
 function SearchPage(props) {
+
+    let [mediaType, setMediaType] = useState("")
+
+    async function handleSubmit(e) {
+        console.log("handling Submit")
+    }
+
     return (
         <div className="search-main">
             <section className="border-solid border-black border-2 w-2/6 h-1/4">
-                <select name="type" id="">
-                    <option value="tv">TV Show</option>
-                    <option value="movie">Movie</option>
-                    <option value="game">Games</option>
-                    <option value="book">Books</option>
-                </select>
+                <div>
+                    <button onClick={() => setMediaType("Movie")}>Movie</button>
+                    <button onClick={() => setMediaType("TV Show")}>Tv Show</button>
+                    <button onClick={() => setMediaType("Game")}>Game</button>
+                    <button onClick={() => setMediaType("Book")}>Book</button>
+                </div>
+                <h3>{mediaType ? `Looking for a ${mediaType}`  : "Choose option to search"}</h3>
                 <br /><br />
                 <label htmlFor="">Search by Name</label>
                 <input type="text" />
-                <button>Search</button>
+                <button onClick={handleSubmit}>Search</button>
             </section>
             <div className="border-solid border-black border-2 flex h-3/5 w-2/3 justify-between">
                 <section className="border-solid border-black border-2 w-2/5">

86ca465445223c45e2abb07bc128a43df7ca68f3
Jordan <higgsfj@gmail.com>
2024-01-28 13:19:31 -0500

    setup conditional for if book search to show author

--- client/src/components/SearchCard.jsx
+++ client/src/components/SearchCard.jsx
@@ -9,6 +9,7 @@ function SearchCard({ con, setSelectedSearch }) {
                 <img src={con.imgURL} alt="No picture found" className='w-full'/>
             </div>
             <h2>{con.name}</h2>
+            {con.author && <h2>Author: {con.author}</h2>}
         </div>
     );
 }
--- client/src/functions/searchArrays.js
+++ client/src/functions/searchArrays.js
@@ -117,7 +117,8 @@ export async function getBookArray(input, setArrayList) {
                 id: result.id,
                 type: "book",
                 name: result.volumeInfo.title,
-                imgURL: result.volumeInfo.imageLinks.thumbnail
+                imgURL: result.volumeInfo.imageLinks.thumbnail,
+                author: result.volumeInfo.authors[0]
             }
             bookArray.push(bookObj)
         }

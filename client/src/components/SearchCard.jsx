5a4a1f9d416754f60558ed7976424b729889d73c
Jordan <higgsfj@gmail.com>
2024-01-30 11:39:48 -0500

    added fallback image functionality in case image doesn't load from API

--- client/src/components/SearchCard.jsx
+++ client/src/components/SearchCard.jsx
@@ -1,9 +1,17 @@
+import { useEffect, useState } from "react";
 import { getGameInfo, getBookInfo, getMovieInfo, getTvInfo } from "../functions/specificSearch";
 
 
 
 function SearchCard({ con, setSelectedSearch }) {
 
+
+    // Handles if image is not found on content
+    function handleError(e) {
+        console.log(e)
+        e.target.onerror = null;
+        e.target.src = "public/img/no_picture.jpg"
+    }
 
     async function handleClick() {
         switch (con.type) {
@@ -31,11 +39,13 @@ function SearchCard({ con, setSelectedSearch }) {
 
     return (
         <div className='border-green-800 border-solid border-2 w-3/12 h-72 hover:bg-green-900 m-4 bg-white' onClick={handleClick}>
-            <div className='w-full'>
-                <img src={con.imgURL} alt="No picture found" className='w-full' />
+            <div className='w-full h-1/2'>
+                <img src={con.imgURL} alt="No picture found" className='w-full h-full' onError={(e) => handleError(e)} />
             </div>
-            <h2>{con.name}</h2>
-            {con.author && <h2>Author: {con.author}</h2>}
+            <div className="text-center">
+                <h2>{con.name}</h2>
+                {con.author && <h2>Author: {con.author}</h2>}
+            </div>
         </div>
     );
 }
